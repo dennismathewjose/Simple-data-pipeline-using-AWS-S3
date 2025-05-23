3accb19a501d77efbc6401f4a3f293b27f173982
Dennis Mathew Jose <dennisparambanattu@gmail.com>
2025-05-23 18:31:32 -0400

    Update sample_test.py

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -8,7 +8,7 @@ def test_process_data_structure():
     # Simulated dataset
     data = {
         'Country name': ['A', 'B', 'C', 'D', 'E', 'F'],
-        'Ladder score': [7.1, 6.2, 6.8, 6.7, 6.6, 6.5],
+        'Ladder score': [7.0, 6.2, 6.8, 6.7, 6.6, 6.5],
         'Generosity': [0.3, 0.25, 0.2, 0.35, 0.1, 0.05]
     }
     df = pd.DataFrame(data)
